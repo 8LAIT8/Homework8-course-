835e516c8656caf82e834cad5bb801224aa67865
lilia <lilia@DESKTOP-64J80RS>
2024-12-09 03:24:26 +0200

    Добавьте файлы проекта.

---
+++ Homework8(course).cpp
@@ -0,0 +1,32 @@
+﻿/*3)
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    unsigned short yourAge;
+
+    unsigned short* ptr = &yourAge;
+                   *ptr = 50;
+
+    cout << "Your age: " << yourAge << endl;
+
+    return 0;
+}
+*/
+/*4)
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int myClassmates;
+
+    int* ptr = &myClassmates;
+        *ptr = 28;
+
+    cout << "The amount of my classmates : " << myClassmates << endl;
+
+    return 0;
+}
+*/
